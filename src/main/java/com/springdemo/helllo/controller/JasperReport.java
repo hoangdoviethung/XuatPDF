0fbaa7d1814de065ad4b9aa0098a856cebb4d983
hung.hoang <hung.hoang@bmbsoft.com.vn>
2022-10-12 10:17:36 +0700

    data postman

--- src/main/java/com/springdemo/helllo/controller/JasperReport.java
+++ src/main/java/com/springdemo/helllo/controller/JasperReport.java
@@ -35,30 +35,115 @@ public class JasperReport {
     }
 
     /*
-         [
+      [{
+    "soDG": "SEATELLER20_FEERE_30092022_1667112484",
+    "ngayGio": "30-09-2022 13:51:51",
+    "user": "SEATELLER20",
+    "bangKe": "",
+    "tenKhachHang": "TRAN THI YEN",
+    "gttt": "163042021",
+    "noiDung": "note",
+    "tongSoTienChi": "9999",
+    "bangChu": "",
+    "ngayCap": "27/10/2020",
+    "maKH": "14892607",
+    "noiCap": "CA NAM DINH dfdfdfdf",
+    "sdt": "0855254514",
+    "diaChi": "22 ngo 210, doi can KHI KI VAM KIM1 LU HAI BAN TRUNG HF",
+    "nguoiNhanTien": "TRAN THI YEN",
+    "nguoiNhapLieu": "SEATELLER20",
+    "nguoiKiemSoat": "SEATELLER20",
+    "nguoiPheDuyet": "SEATELLER20",
+    "coCode": "Chi nhánh Sở giao dịch (VN0010002)",
+    "type": "EURO"
+},
+{
+    "soDG": "SEATELLER20_FEERE_30092022_1667112484",
+    "ngayGio": "30-09-2022 13:51:51",
+    "user": "SEATELLER20",
+    "bangKe": "",
+    "tenKhachHang": "TRAN THI YEN",
+    "gttt": "163042021",
+    "noiDung": "note",
+    "tongSoTienChi": "9999",
+    "bangChu": "",
+    "ngayCap": "27/10/2020",
+    "maKH": "14892607",
+    "noiCap": "CA NAM DINH dfdfdfdf",
+    "sdt": "0855254514",
+    "diaChi": "22 ngo 210, doi can KHI KI VAM KIM1 LU HAI BAN TRUNG HF",
+    "nguoiNhanTien": "TRAN THI YEN",
+    "nguoiNhapLieu": "SEATELLER20",
+    "nguoiKiemSoat": "SEATELLER20",
+    "nguoiPheDuyet": "SEATELLER20",
+    "coCode": "Chi nhánh Sở giao dịch (VN0010002)",
+    "type": "EURO"
+},
+{
+    "soDG": "SEATELLER20_FEERE_30092022_1667112484",
+    "ngayGio": "30-09-2022 13:51:51",
+    "user": "SEATELLER20",
+    "bangKe": "",
+    "tenKhachHang": "TRAN THI YEN",
+    "gttt": "163042021",
+    "noiDung": "note",
+    "tongSoTienChi": "9999",
+    "bangChu": "",
+    "ngayCap": "27/10/2020",
+    "maKH": "14892607",
+    "noiCap": "CA NAM DINH dfdfdfdf",
+    "sdt": "0855254514",
+    "diaChi": "22 ngo 210, doi can KHI KI VAM KIM1 LU HAI BAN TRUNG HF",
+    "coCode": "Chi nhánh Sở giao dịch (VN0010002)",
+    "loaiBangKe": "Bảng kê các loại tiền lĩnh (VND)",
+    "type": "VND",
+    "fields": [
         {
-            "soDG": "SEATELLER20_FEERE_30092022_1667112484",
-            "ngayGio": "30-09-2022 13:51:51",
-            "user": "SEATELLER20",
-            "bangKe": "",
-            "tenKhachHang": "TRAN THI YEN",
-            "gttt": "163042021",
-            "noiDung": "note",
-            "tongSoTienChi": "9999",
-            "bangChu": "",
-            "ngayCap": "27/10/2020",
-            "maKH": "14892607",
-            "noiCap": "CA NAM DINH dfdfdfdf",
-            "sdt": "0855254514",
-            "diaChi": "22 ngo 210, doi can KHI KI VAM KIM1 LU HAI BAN TRUNG HF",
-            "nguoiNhanTien": "TRAN THI YEN",
-            "nguoiNhapLieu": "SEATELLER20",
-            "nguoiKiemSoat": "SEATELLER20",
-            "nguoiPheDuyet": "SEATELLER20",
-            "coCode": "Chi nhánh Sở giao dịch (VN0010002)"
+            "menhGia": "500000",
+            "soTo": "5"
+        },
+        {
+            "menhGia": "200000",
+            "soTo": null
+        },
+        {
+            "menhGia": "100000",
+            "soTo": null
+        },
+        {
+            "menhGia": "50000",
+            "soTo": "5"
+        },
+        {
+            "menhGia": "20000",
+            "soTo": "5277"
+        },
+        {
+            "menhGia": "10000",
+            "soTo": "1"
+        },
+        {
+            "menhGia": "5000",
+            "soTo": "15"
+        },
+        {
+            "menhGia": "2000",
+            "soTo": "3"
+        },
+        {
+            "menhGia": "1000",
+            "soTo": "6"
+        },
+        {
+            "menhGia": "500",
+            "soTo": "3"
         }
-    ]
-
-    *?
+    ],
+    "nguoiNhanTien": "TRAN THI YEN",
+    "nguoiNhapLieu": "SEATELLER20",
+    "nguoiKiemSoat": "SEATELLER20",
+    "nguoiPheDuyet": "SEATELLER20"
+}
+]
      */
 }
